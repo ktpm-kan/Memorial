a080e6efc2ad2b8d642fb4ae0a89b3318d503c62
BuildTools <unconfigured@null.spigotmc.org>
2019-11-21 02:04:29 +0900

    ルート見学 リスト完成！！！

---
+++ app/src/main/java/com/example/memorial_app/MyAdapter.java
@@ -0,0 +1,72 @@
+package com.example.memorial_app;
+
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import java.util.List;
+
+public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
+
+    private List<Integer> iImages;
+    private List<String> iNames;
+    private List<String> iEmails;
+
+    // Provide a reference to the views for each data item
+    // Complex data items may need more than one view per item, and
+    // you provide access to all the views for a data item in a view holder
+    static class ViewHolder extends RecyclerView.ViewHolder {
+
+        // each data item is just a string in this case
+        ImageView imageView;
+        TextView textView;
+        TextView emailView;
+
+        ViewHolder(View v) {
+            super(v);
+            imageView = v.findViewById(R.id.image_view);
+            textView = v.findViewById(R.id.text_view);
+            emailView = v.findViewById(R.id.email_view);        }
+    }
+
+    // Provide a suitable constructor (depends on the kind of dataset)
+    MyAdapter(List<Integer> itemImages, List<String> itemNames, List<String> itemEmails) {
+        this.iImages = itemImages;
+        this.iNames = itemNames;
+        this.iEmails = itemEmails;
+    }
+
+    // Create new views (invoked by the layout manager)
+    @Override
+    @NonNull
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        // create a new view
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.my_text_view, parent, false);
+
+        // set the view's size, margins, paddings and layout parameters
+
+        return new ViewHolder(view);
+    }
+
+    // Replace the contents of a view (invoked by the layout manager)
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        // - get element from your dataset at this position
+        // - replace the contents of the view with that element
+        holder.imageView.setImageResource(iImages.get(position));
+        holder.textView.setText(iNames.get(position));
+        holder.emailView.setText(iEmails.get(position));    }
+
+    // Return the size of your dataset (invoked by the layout manager)
+    @Override
+    public int getItemCount() {
+        return iNames.size();
+    }
+}
--- app/src/main/java/com/example/memorial_app/RouteChoiceSpotActivity.java
+++ app/src/main/java/com/example/memorial_app/RouteChoiceSpotActivity.java
@@ -6,18 +6,75 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Locale;
+
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.recyclerview.widget.LinearLayoutManager;
+
+
 public class RouteChoiceSpotActivity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_route_choice_spot);
-    }
 
     public void onButton1(View v) {
         // 画面指定
         Intent intent = new Intent(this,RouteMapActivity.class);
         // 画面を開く
         startActivity(intent);
+    }
+
+    // *************************************************
+    // RecyclerView
+    // *************************************************
+    private static final String[] names = {
+            "test00", "test01", "test02",
+            "test03", "test04", "test05",
+            "test06", "test07", "test08", "test09"
+    };
+
+    // それぞれの画像ファイルをdarawableに入れます
+    // ArrayListにコピーするためintからInteger型にしました
+    private static final Integer[] photos = {
+            R.drawable.img00, R.drawable.img01,
+            R.drawable.img02, R.drawable.img03,
+            R.drawable.img04, R.drawable.img05,
+            R.drawable.img06, R.drawable.img07,
+            R.drawable.img08, R.drawable.img09
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_route_choice_spot);
+        // activity_route_choice_spot の id と 合ってるか確認
+
+        RecyclerView recyclerView = findViewById(R.id.my_recycler_view);
+
+        // use this setting to improve performance if you know that changes
+        // in content do not change the layout size of the RecyclerView
+        recyclerView.setHasFixedSize(true);
+
+        // use a linear layout manager
+        RecyclerView.LayoutManager rLayoutManager = new LinearLayoutManager(this);
+
+        recyclerView.setLayoutManager(rLayoutManager);
+
+
+        // 配列をArrayListにコピー
+        List<String> itemNames = new ArrayList<String>(Arrays.asList(names));
+        List<Integer> itemImages = new ArrayList<Integer>(Arrays.asList(photos));
+
+        List<String> itemEmails = new ArrayList<String>();
+        for(int i=0; i<itemNames.size() ;i++ ){
+            String str = String.format(Locale.ENGLISH, "%s@gmail.com", itemNames.get(i));
+            itemEmails.add(str);
+        }
+
+        // specify an adapter (see also next example)
+        RecyclerView.Adapter rAdapter = new MyAdapter(itemImages, itemNames, itemEmails);
+        recyclerView.setAdapter(rAdapter);
+
     }
 }
