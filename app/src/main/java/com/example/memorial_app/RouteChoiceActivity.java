cab4360f657c3e9e491839f47462ba3b8c92bdc9
BuildTools <unconfigured@null.spigotmc.org>
2019-11-20 21:14:03 +0900

    ルート見学 リスト作成前

--- app/src/main/java/com/example/memorial_app/RouteChoiceActivity.java
+++ app/src/main/java/com/example/memorial_app/RouteChoiceActivity.java
@@ -2,6 +2,7 @@ package com.example.memorial_app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
@@ -12,6 +13,20 @@ public class RouteChoiceActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_route_choice);
+    }
+
+    public void onButton1(View v) {
+        // 画面指定
+        Intent intent = new Intent(this,RouteChoiceTimeActivity.class);
+        // 画面を開く
+        startActivity(intent);
+    }
+
+    public void onButton2(View v) {
+        // 画面指定
+        Intent intent = new Intent(this,RouteChoiceSpotActivity.class);
+        // 画面を開く
+        startActivity(intent);
     }
 
     public void onClose(View v) {
---
+++ app/src/main/java/com/example/memorial_app/RouteChoiceSpotActivity.java
@@ -0,0 +1,14 @@
+package com.example.memorial_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RouteChoiceSpotActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_route_choice_spot);
+    }
+}
