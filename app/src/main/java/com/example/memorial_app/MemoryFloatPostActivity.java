56f3471d16ea748e1be53f0f6b4a7d083a935993
BuildTools <unconfigured@null.spigotmc.org>
2020-01-22 00:57:02 +0900

    メモリーフロート 投稿フォーム 確認画面完成

--- app/src/main/java/com/example/memorial_app/MemoryFloatPostActivity.java
+++ app/src/main/java/com/example/memorial_app/MemoryFloatPostActivity.java
@@ -2,13 +2,27 @@ package com.example.memorial_app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MemoryFloatPostActivity extends AppCompatActivity {
+
+    public static final String NICK_NAME = "com.example.myfirstapp.MESSAGE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_memory_float_post);
+    }
+
+    /** Called when the user taps the Send button */
+    public void sendMessage(View view) {
+        Intent intent = new Intent(this, MemoryFloatPostConfirmationActivity.class);
+        EditText editText = (EditText) findViewById(R.id.NickName);
+        String message = editText.getText().toString(); // エディットテキストのテキストを取得
+        intent.putExtra(NICK_NAME, message);            // EditText の値をインテントに追加します
+        startActivity(intent);                          //インテントで指定されたアクティビティ開始
     }
 }
--- app/src/main/java/com/example/memorial_app/MemoryFloatPostConfirmationActivity.java
+++ app/src/main/java/com/example/memorial_app/MemoryFloatPostConfirmationActivity.java
@@ -2,13 +2,29 @@ package com.example.memorial_app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
+
 
 public class MemoryFloatPostConfirmationActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_memory_float_post_confirmation);
+
+        // Get the Intent that started this activity and extract the string
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(MemoryFloatPostActivity.NICK_NAME);
+
+        // Capture the layout's TextView and set the string as its text
+        TextView textView = findViewById(R.id.NickNameData);
+        textView.setText(message);
+
     }
+
+
 }
